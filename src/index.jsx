a24fc32230df1801639eea321f9c76d65657facd
ulises-david <ulisesdavidacosta27@gmail.com>
2022-02-04 10:51:44 -0300

    trabajo terminado

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HelloUser from'./HelloUser'
+
+ReactDOM.render(
+    <HelloUser />,
+  document.getElementById('root')
+);
